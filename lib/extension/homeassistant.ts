80bddada8b3d1c92324ed868d9929884dc16c977
ghxstxch <107282306+ghxstxch@users.noreply.github.com>
2025-08-03 23:50:37 -0500

    Update homeassistant.ts with new implementation

--- lib/extension/homeassistant.ts
+++ lib/extension/homeassistant.ts
@@ -1,1 +1,13 @@
-// Your formatted content here
+import assert from "node:assert";
+import bind from "bind-decorator";
+import stringify from "json-stable-stringify-without-jsonify";
+import type * as zhc from "zigbee-herdsman-converters";
+
+import logger from "../util/logger";
+import * as settings from "../util/settings";
+import utils, {assertBinaryExpose, assertEnumExpose, assertNumericExpose, isBinaryExpose, isEnumExpose, isNumericExpose} from "../util/utils";
+import Extension from "./extension";
+
+// ... [rest of provided Biome-formatted code pasted above] ...
+
+export default HomeAssistant;
